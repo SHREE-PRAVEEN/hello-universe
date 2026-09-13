dfbabab3e80445c174f2edcd9a51053f3a8416b0
SHREE-PRAVEEN <kpraveen206@yahoo.com>
2026-09-14 03:16:55 +0530

    update

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -36,7 +36,7 @@ impl Config {
         dotenvy::dotenv().ok();
         Self {
             database_url: env::var("DATABASE_URL")
-                .unwrap_or_else(|_| "postgres://shreepraveen:Shreepraveen%4023@localhost:5432/hello_universe".into()),
+                .unwrap_or_else(|_| "postgresql://neondb_owner:npg_5xkRD6vVCruf@ep-green-block-a5unm8r6-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require".into()),
             port: env::var("PORT").ok().and_then(|value| value.parse().ok()).unwrap_or(8080),
             db_max_connections: env::var("DB_MAX_CONNECTIONS")
                 .ok()
